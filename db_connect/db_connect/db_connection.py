501457ad819c6308e872b812d1b909f85568b46d
ziyu <ziyu.l@helium10.com>
2021-07-14 16:43:23 -0700

    db_connection

---
+++ db_connect/db_connect/db_connection.py
@@ -0,0 +1,40 @@
+import boto3
+import pandas as pd
+import psycopg2
+import mysql.connector
+import sshtunnel
+import json
+
+with open('./Desktop/ETL_Script/credential.json') as json_file:
+    credential = json.load(json_file)
+
+def db_connect_pgsql(database):
+    param_dic_h10 = {
+    "host" : credential[database]["host"],
+    "user" : credential[database]["user"],
+    "database" : credential[database]["database"],
+    "password": credential[database]["password"]
+    }
+
+    conn = psycopg2.connect(**param_dic_h10)
+
+    return conn
+
+def db_connect_mysql(remote_bind_address,database,query):
+    with sshtunnel.SSHTunnelForwarder(
+        ('3.91.34.122', 22),
+        ssh_username = 'ziyu.li',
+        ssh_password = '/Users/ziyuli/.ssh/id_rsa',
+        remote_bind_address = (remote_bind_address, 3306)
+    ) as tunnel:
+        conn = mysql.connector.MySQLConnection(
+            host='127.0.0.1',
+            user="ziyu.li",
+            password=credential["mysql"]["password"],
+            port=tunnel.local_bind_port,
+            database = database)
+        cursor = conn.cursor()
+        cursor.execute(query)
+        df = pd.DataFrame(cursor.fetchall())
+
+    return df
